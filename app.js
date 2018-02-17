76654660bc397ceb0f1673a08604786e6cce97af
Dominik Wagner <dominik.waagna@gmail.com>
2018-02-17 20:20:32 +0100

    Added Db connection and show route

--- app.js
+++ app.js
@@ -1,34 +1,83 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: 'Grubhof', image: 'https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb'},
-    {name: 'Wilder Kaiser', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb'},
-    {name: 'Camping Arlberg', image: 'https://images.pexels.com/photos/6714/light-forest-trees-morning.jpg?h=350&dpr=2&auto=compress&cs=tinysrgb'}
-];
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: 'Wilder Kaiser', 
+//     image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb', 
+//     description: "Es ist kein Lager, sondern ein Wanderhotel. Es gibt eine Euro-Camp Wilder Kaiser in Kössen!"
+// }, (err, campground) => {
+//     if(err) {
+//         console.log(err);
+//     }else {
+//         console.log("new created campground: ");
+//         console.log(campground);
+//     }
+// });
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
+// Index -> Show all Campgrounds
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, (err, allcampgrounds) => {
+        if (err) {
+            console.log(err);
+        }else {
+            res.render("index", {campgrounds: allcampgrounds})
+        }
+    });
 });
 
+// Create -> add new campground to db
 app.post("/campgrounds", (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
-    campgrounds.push({name: name, image: image});
+    var desc = req.body.description;
 
-    res.redirect("/campgrounds");
+    //Save new Campground to db
+    Campground.create({name: name, image: image, description: desc}, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        }else {
+            res.redirect("/campgrounds");
+        }
+    });
+    Campground.remove()
+
 });
 
+// NEW -> Display the form for a new campground
 app.get("/campgrounds/new", (req, res) => {
     res.render("new");
+});
+
+// Show -> Shows more info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+    // res.send("This will be the show page");
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if (err){
+            console.log(err)
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
+    
 });
 
 app.listen(3000, "localhost", _ => {
