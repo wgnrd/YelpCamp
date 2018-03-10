220c7c44dad3c15a176afb2c0284410f707b095c
Dominik Wagner <dominik.waagna@gmail.com>
2018-03-10 13:17:22 +0100

    Removed listening on localhost only

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, () => {
     console.log(`We are listening on ${PORT}`);
 });
 
